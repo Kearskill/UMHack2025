69a92d5bb04f483e16294fb4c2160633a8aec40a
Kearskill <kearskill@gmail.com>
2025-04-09 02:59:11 +0800

    hello world

--- app.py
+++ app.py
@@ -1,2 +1,11 @@
-# file kosong
 # Streamlit frontend
+import streamlit as st
+
+# streamlit run app.py
+
+st.set_page_config(page_title="Hello Worl App", layout="centered")
+
+st.markdown(
+    "<h1 style='text-align: center; font-size: 80px;'>Helo, World! 🌍</h1>",
+    unsafe_allow_html=True
+)
