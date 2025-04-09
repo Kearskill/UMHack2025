df353fc109366ed4e41f368e52810efc57c9beb3
Kearskill <kearskill@gmail.com>
2025-04-09 15:15:04 +0800

    testing

--- cybo_api.py
+++ cybo_api.py
@@ -1,2 +1,7 @@
-# file kosong
-# call api, kena belajar env file
+# import api key
+import os
+from dotenv import load_dotenv
+load_dotenv()
+api_key = os.getenv("X-API-KEY")
+
+print(api_key) # testing
